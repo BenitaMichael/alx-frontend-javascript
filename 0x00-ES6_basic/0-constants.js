66f21e0e418bc19a2096e544ff07e202a5bc7116
Benita_Michael <benubiaka@gmail.com>
2024-03-08 10:03:48 +0000

    Task

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,15 @@
-export default function taskFirst(){
-	const task = "I prefer const when I can.";
-	return task;
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
 }
 
-export default function getLast(){
-	return 'is okay';
+export function getLast() {
+  return 'is okay';
 }
 
-export default function taskNext(){
-	let combination = "But sometimes let"
-	combination += getLast();
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
 
-	return combination;
+  return combination;
 }
